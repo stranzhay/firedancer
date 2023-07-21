b92990aeb894812f41cc75a6a9cf66854202589c
Richard Patel <ripatel@jumptrading.com>
2023-07-21 00:19:22 +0000

    flamenco: add library init boilerplate

---
+++ src/flamenco/fd_flamenco.c
@@ -0,0 +1,10 @@
+#include "fd_flamenco.h"
+
+/* In anticipation of future flamenco initialization logic ... */
+
+void
+fd_flamenco_boot( int *    pargc __attribute__((unused)),
+                  char *** pargv __attribute__((unused)) ) {}
+
+void
+fd_flamenco_halt( void ) {}
