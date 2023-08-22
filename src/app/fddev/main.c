948fa282badb6a121f1229626bf0df52cd9fd9c7
Michael McGee <mmcgee@jumptrading.com>
2023-08-22 20:46:33 +0000

    solana: bank

--- src/app/fddev/main.c
+++ src/app/fddev/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 
 configure_stage_t * STAGES[ CONFIGURE_STAGE_COUNT ] = {
   &netns,
@@ -26,6 +27,7 @@ configure_stage_t * STAGES[ CONFIGURE_STAGE_COUNT ] = {
 static action_t DEV_ACTIONS[] = {
   { .name = "dev",  .args = dev_cmd_args,  .fn = dev_cmd_fn,  .perm = dev_cmd_perm },
   { .name = "dev1", .args = dev1_cmd_args, .fn = dev1_cmd_fn, .perm = dev_cmd_perm },
+  { .name = "txn",  .args = txn_cmd_args,  .fn = txn_cmd_fn,  .perm = txn_cmd_perm },
 };
 
 #define MAX_ARGC 32
