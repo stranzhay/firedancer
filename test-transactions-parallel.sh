5303110a29f1eb50658cd70a5797aed38b47b450
Logan Lamb <llamb@jumptrading.com>
2023-08-23 15:51:37 +0000

    This is using the rust version of test_quic_txn: https://github.com/firedancer-io/firedancer-playground/tree/main/test_quic_txn
    llamb@emny-ossdev-firedancer11> time ./test-transactions-parallel.sh
     8102 / 8102 successfully transmitted by clients (64 at a time)
    ./test-transactions-parallel.sh  112.36s user 187.48s system 1100% cpu 27.249 totalm

--- test-transactions-parallel.sh
+++ test-transactions-parallel.sh
@@ -5,8 +5,8 @@ IFS=$'\n\t'
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}"
 rm -f /tmp/run-*
-NUM_TRANSACTIONS=${1:-1000}
-TX_FILE=${2:-tx}
+NUM_TRANSACTIONS=${1:-8102}
+TX_FILE=${2:-all.txns}
 NUM_JOBS=${3:-64}
 set +e
 head -n $NUM_TRANSACTIONS $TX_FILE | parallel -j $NUM_JOBS "sudo nsenter --net=/var/run/netns/veth_test_xdp_1 /home/llamb/code/firedancer-playground/test_quic_txn/target/debug/test_quic_txn --payload-base64-encoded {} > /tmp/run-{#} 2>&1"
