52685bc8692ec0383c5e31b5d71630af583cb33c
Sarah Strange <strange1120@gmail.com>
2023-08-29 01:28:50 +0000

    ffi: add binding functions to reedsol crate

---
+++ ffi/rust/firedancer-sys/src/ballet/reedsol.rs
@@ -0,0 +1,9 @@
+pub use crate::generated::{
+    fd_reedsol_align, fd_reedsol_encode_abort, fd_reedsol_encode_add_data_shred,
+    fd_reedsol_encode_add_parity_shred, fd_reedsol_encode_fini, fd_reedsol_encode_init,
+    fd_reedsol_footprint, fd_reedsol_footprint, fd_reedsol_recover_abort,
+    fd_reedsol_recover_add_erased_shred, fd_reedsol_recover_add_rcvd_shred,
+    fd_reedsol_recover_fini, fd_reedsol_recover_init, fd_reedsol_strerror, FD_REEDSOL_ALIGN,
+    FD_REEDSOL_DATA_SHREDS_MAX, FD_REEDSOL_ERR_CORRUPT, FD_REEDSOL_ERR_PARTIAL,
+    FD_REEDSOL_FOOTPRINT, FD_REEDSOL_PARITY_SHREDS_MAX, FD_REEDSOL_SUCCESS,
+};
